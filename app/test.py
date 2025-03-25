b33562d59bdecf446950cce8c18f00e1d895f87c
rromero09 <rafael.rromero97@gmail.com>
2025-03-25 11:07:18 -0500

    Refactor environment variable loading and update dependencies

--- app/test.py
+++ app/test.py
@@ -1,6 +1,5 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-print("MONGO_URI:", os.getenv("MONGO_URI"))
-print("DB_NAME:", os.getenv("DB_NAME"))
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("DB_NAME")
+print(MONGO_URI, DB_NAME)
