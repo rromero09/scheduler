44c1dacf08a7654ced18db6290d8247f2a81e4f0
Ralph <rromero8@neiu.edu>
2025-03-28 19:39:51 -0500

    Update mongo.py

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -7,6 +7,11 @@ load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME")
+print("MONGO_URI:", MONGO_URI)
+print("DB_NAME:", DB_NAME)
 # Connect to MongoDB
 client = MongoClient(MONGO_URI)
 db = client[DB_NAME]
+def get_workers_from_db(): # fake function to simulate fetching workers from a database
+    workers = ["nazik", "nabor", "maher", "maria", "valeria", "charlie","eduardo", "rafa"]
+    return workers    # Return the list of workers as a response
